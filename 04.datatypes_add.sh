fa45ee5f7fa44c1b1708f4f7c470b934918046de
raju2 <raj.sresta@gmail.com>
2024-07-01 10:45:10 +0530

    In frontend,we are installing nginx

--- 04.datatypes_add.sh
+++ 04.datatypes_add.sh
@@ -5,5 +5,9 @@ echo "Enter the second number"
 read -s secondnumber
 result=$((firstbumber+secondnumber))
 echo "The sum of entered numbers is: ${result}"
-
-
+result_diff=$((firstbumber-secondnumber))
+echo "The sum of entered numbers is: ${result_diff}"
+result_prod=$((firstbumber*secondnumber))
+echo "The sum of entered numbers is: ${result_prod}"
+result_quo=$((firstbumber/secondnumber))
+echo "The sum of entered numbers is: ${result_quo}"
