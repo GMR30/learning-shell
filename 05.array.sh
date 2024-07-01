a6f8f94434e24a82829c98ecc5f1221b9bb5fc50
raju2 <raj.sresta@gmail.com>
2024-07-01 11:03:39 +0530

    In frontend,we are installing nginx

--- 05.array.sh
+++ 05.array.sh
@@ -2,4 +2,4 @@
 languages=("Telugu" "Hindi" "English" "Tamil" "Kannada")
 echo "The first language is: ${languages[0]}"
 echo "The language are: ${languages[@]}"
-echo "The total no of languages is: ${languages[#]}"
+echo "The total no of languages is: $[#]}"
