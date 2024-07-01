df2743cc80da703fbecc3da5c2f25d5039eaa2b4
raju2 <raj.sresta@gmail.com>
2024-07-01 10:30:21 +0530

    In frontend,we are installing nginx

--- 02.variables.sh
+++ 02.variables.sh
@@ -12,3 +12,8 @@ Welcome=$2
 How=$3
 echo "Hello $1,$2,$3"
 
+echo "Hello ${name},${Welcome},${How}"
+
+#to know no of arguments passed and what are they ?
+echo "The no of arguments is : $#"
+echo "The arguments are: $@ "
