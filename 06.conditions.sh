ff10ab73a137160bd76c1a1bedc623df1f77870a
raju2 <raj.sresta@gmail.com>
2024-07-01 11:34:23 +0530

    In frontend,we are installing nginx

---
+++ 06.conditions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+USERID=$(id -u)
+if [$USERID -eq 0 ]
+then
+  echo "user has root access"
+else
+  echo "User has not root privileges"
+  exit 127
+fi
+echo "Installing java"
+yum install java -y
+if [$? -eq 0]
+then
+  echo "successfully installed java application."
+else
+  echo "We are NOT installed the requested Java application."
+  exit 127
+fi
