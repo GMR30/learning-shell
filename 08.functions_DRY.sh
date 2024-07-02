b843e087bd80fc56fbd15a5e0873c12ce02a69af
raju2 <raj.sresta@gmail.com>
2024-07-02 11:13:34 +0530

    In frontend,we are installing nginx

--- 08.functions_DRY.sh
+++ 08.functions_DRY.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 userid=$(id -u)
+Date=$(date + "%F-%H-%M-%S" )
+Log_File="$Date.log"
 if [ $userid -ne 0 ]
 then
    echo "You are not root user,please run the script with root user"
@@ -14,16 +16,16 @@ validate(){
     echo "$2 ..is SUCCESS "
    fi
 }
-yum install git -y
+yum install git -y &>>Log_File
 validate $? "GIT installation"
 
-yum install java -y
+yum install java -y &>>Log_File
 validate $? "JAVA installation"
 
-yum install net-tools -y
+yum install net-tools -y &>>Log_File
 validate $? "NET-TOOLS installation"
 
-yum install wget -y
+yum install wget -y &>>Log_File
 validate $? "WGET installation"
 
 
