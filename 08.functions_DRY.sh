94799fd4a54e125bfb3c564ba2c4dfa871d1a53a
raju2 <raj.sresta@gmail.com>
2024-07-02 10:43:57 +0530

    In frontend,we are installing nginx

---
+++ 08.functions_DRY.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+userid=$(id -u)
+if [ $userid -ne 0 ]
+then
+   echo "You are not root user,please run the script with root user"
+   exit 127
+fi
+validate(){
+   if [ $1 -ne 0 ]
+  then
+    echo "$2 ..is FAILED "
+    exit 127
+  else
+    echo "$2 ..is SUCCESS "
+   fi
+}
+yum install git -y
+validate $? "GIT installation"
+
+yum install java -y
+validate $? "JAVA installation"
+
+yum install net-tools -y
+validate $? "NET-TOOLS installation"
+
+yum install wget -y
+validate $? "WGET installation"
