a98c0053672b7440c69c3b0030dd7a75669f1d89
raju2 <raj.sresta@gmail.com>
2024-07-02 10:01:01 +0530

    In frontend,we are installing nginx

---
+++ 07.functions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+userid=$(id -u)
+if [ $userid -ne 0 ]
+then
+   echo "You are not root user,please run the script with root user"
+   exit 127
+fi
+echo "installing Git"
+
+yum install git -y
+
+if [ $? -ne 0]
+then
+  echo "Git had failed to install"
+  exit 127
+else
+  echo "Git is Successfully installed"
+fi
+
+
