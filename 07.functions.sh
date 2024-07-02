0cd09b1b2b546b2e052377694d8011d21c6b63f3
raju2 <raj.sresta@gmail.com>
2024-07-02 10:11:56 +0530

    In frontend,we are installing nginx

--- 07.functions.sh
+++ 07.functions.sh
@@ -17,4 +17,40 @@ else
   echo "Git is Successfully installed"
 fi
 
+echo "installing java "
+
+yum install java -y
+
+if [ $? -ne 0 ]
+then
+  echo "java had failed to install"
+  exit 127
+else
+  echo "java is Successfully installed"
+fi
+
+echo "installing net-tools"
+
+yum install net-tools -y
+
+if [ $? -ne 0 ]
+then
+  echo "net-tools had failed to install"
+  exit 127
+else
+  echo "net-tools is Successfully installed"
+fi
+
+echo "installing wget"
+
+yum install wget -y
+
+if [ $? -ne 0 ]
+then
+  echo "wget had failed to install"
+  exit 127
+else
+  echo "wget is Successfully installed"
+fi
+
 
