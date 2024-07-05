cb28df167c0a1b66ee8667ebdb06247d403e2e9c
raju2 <raj.sresta@gmail.com>
2024-07-05 10:08:45 +0530

    In frontend,we are installing nginx

---
+++ 16.remove-oldlogs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+Date=$(date "%F-%H-%M-%S")
+path=/home/ec2-user/dailylogs
+Log-File="$Date.log"
+input=$(find /home/ec2-user/dailylogs/ -type f -mtime  +14)
+echo "output is :${input}"
+while IFS= read line;
+do
+  #rm -rf  &>>$Log-File
+  echo "Deleting the line: $line"
+done<<<"$input"
+
+
+
