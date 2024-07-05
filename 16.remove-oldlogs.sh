0fb188ad9c6d369e9cd93f6b44d2e0ede2f7fa4e
raju2 <raj.sresta@gmail.com>
2024-07-05 10:25:03 +0530

    In frontend,we are installing nginx

--- 16.remove-oldlogs.sh
+++ 16.remove-oldlogs.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 Date=$(date +"%F-%H-%M-%S")
 Path=/home/ec2-user/dailylogs
-Log_File1="$Date.log"
+Log_File="$Date.log"
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
 Input=$(find /home/ec2-user/dailylogs/ -type f -mtime  +14)
-echo -e "$G output is$N :${Input}"
+echo -e "$G output is$N :${Input}" &>>$Log_File
 while IFS= read line;
 do
   #rm -rf  &>>$Log-File
-  echo -e "$R Deleting the line:$N $line "
+  echo -e "$R Deleting the line:$N $line " &>>$Log_File
 done<<<"$Input"
 
 
