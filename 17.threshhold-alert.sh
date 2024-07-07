ea4fdc26d1a92ca24fd8ec226a10bfabc662f3ba
raju2 <raj.sresta@gmail.com>
2024-07-07 11:46:15 +0530

    In frontend,we are installing nginx

---
+++ 17.threshhold-alert.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#df -hT | grep -vE 'tmpfs|Filesystem' | awk '{print $6}'
+#df -hT | grep -vE 'tmpfs|Filesystem' | awk '{print $6}' | cut -d "%" -f1
+
+Disk_threshold=10
+Disk_usage=$(df -hT | grep -vE 'tmpfs|Filesystem' | awk '{print $6}' | cut -d "%" -f1)
+message=""
+while IFS= read line;
+do
+  usage=$(echo $line | cut -d "%" -f1)
+  partition==$(echo $Disk_usage | cut -d " " f2)
+  echo "usage: $usage"
+  echo "partition: $partition"
+  if[ $usage -ge $Disk_threshold ]
+  then
+      message+="High disk usage on $partition: $usage%\n"
+  fi
+  done<<<"$Disk_usage"
+  echo "message:$message"
+  echo -e "$message" | mail -s "High Disk usage" info@subbannadairy.com
+
+
